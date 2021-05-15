692062eb540062fa52f5de0cacb4a9de07b4a1ee
Crobinson17 <79876543+Crobinson17@users.noreply.github.com>
2021-05-14 22:54:27 -0600

    Titles table

---
+++ DataEngineering/Titles.sql
@@ -0,0 +1,14 @@
+CREATE TABLE public.titles
+(
+    title_id varchar NOT NULL,
+    title varchar NOT NULL,
+    PRIMARY KEY (title_id)
+);
+
+ALTER TABLE public.titles
+    OWNER to postgres;
+	
+
+	
+
+
